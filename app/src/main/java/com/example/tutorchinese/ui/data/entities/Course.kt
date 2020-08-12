481212cee5f3dea7aa30d8b139e99aeeb954d207
toey <annop.tsg@gmail.com>
2020-08-13 00:27:49 +0700

    ทำที่ห้อง

--- app/src/main/java/com/example/tutorchinese/ui/data/entities/Course.kt
+++ app/src/main/java/com/example/tutorchinese/ui/data/entities/Course.kt
@@ -4,6 +4,8 @@ data class Course (
     var Cr_id: Int?,
     var T_id: Int?,
     var Cr_name: String? = null,
+    var Cr_link: String? = null,
+    var Cr_file: String? = null,
     var Cr_info: String? = null,
     var Cr_price: String? = null,
     var Cr_data_time: String? = null
--- app/src/main/java/com/example/tutorchinese/ui/view/course/add_course/AddCourseFragment.kt
+++ app/src/main/java/com/example/tutorchinese/ui/view/course/add_course/AddCourseFragment.kt
@@ -1,7 +1,13 @@
 package com.example.tutorchinese.ui.view.course.add_course
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,16 +20,20 @@ import com.example.tutorchinese.R
 import com.example.tutorchinese.ui.controler.PreferencesData
 import com.example.tutorchinese.ui.view.main.MainActivity
 
+
 class AddCourseFragment : Fragment(), View.OnClickListener {
 
     private var supportFragmentManager: FragmentManager? = null
     private lateinit var addCoursePresenter: AddCoursePresenter
     private var user: PreferencesData.Users? = null
 
+    private val FILE_SELECT_CODE = 1001
+
     //layout
     private var edtCourseName: EditText? = null
     private var edtCoursePrice: EditText? = null
     private var edtCourseDetail: EditText? = null
+    private var edtCourseLink: EditText? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,11 +63,18 @@ class AddCourseFragment : Fragment(), View.OnClickListener {
 
 
         val btnSaveCourse: Button = root.findViewById(R.id.btnSaveCourse)
+        val btnCourseFile: Button = root.findViewById(R.id.btnCourseFile)
         edtCourseName = root.findViewById(R.id.edtCourseName)
         edtCoursePrice = root.findViewById(R.id.edtCoursePrice)
         edtCourseDetail = root.findViewById(R.id.edtCourseDetail)
+        edtCourseLink = root.findViewById(R.id.edtCourseLink)
+
 
         btnSaveCourse.setOnClickListener(this)
+        btnCourseFile.setOnClickListener(this)
+
+
+
     }
 
     override fun onClick(p0: View) {
@@ -77,8 +94,56 @@ class AddCourseFragment : Fragment(), View.OnClickListener {
                     }
                 }
             }
+            R.id.btnCourseFile->{
+                chooseFile()
+            }
         }
     }
+
+    private fun chooseFile() {
+
+
+
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            startActivityForResult(
+                Intent.createChooser(intent, "เลือกไฟล์"),
+                FILE_SELECT_CODE
+            )
+        } catch (ex: ActivityNotFoundException) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(
+                activity, "กรุณาติดตั้ง File Manager..",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        when (requestCode) {
+            FILE_SELECT_CODE -> {
+                // Get the Uri of the selected file
+                val uri = data?.data!!
+                Log.d("as685d12asd", "File Uri: $uri")
+                // Get the path
+
+
+                val path: String = convertMediaUriToPath(uri)!!
+                Log.d("as685d12asd", "File Path: $path")
+                // Get the file instance
+                // File file = new File(path);
+                // Initiate the upload
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
+
 
     @SuppressLint("SetTextI18n")
     private fun manageToolbar() {
